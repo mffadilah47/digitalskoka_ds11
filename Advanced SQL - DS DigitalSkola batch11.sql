1cbf87b5c970e7c7a4ad02f4781b16de84d86a02
Muhammad Fikri Fadila <muh.fikrifadila@gmail.com>
2022-04-03 16:59:19 +0700

    Create code for showing first 100 fibonacii numbers

--- Advanced SQL - DS DigitalSkola batch11.sql
+++ Advanced SQL - DS DigitalSkola batch11.sql
@@ -13,22 +13,37 @@ group by c.gender, c.city, c.email;
 
 select product_id, sum(quantity) as total_transactions
 from datasource_sql_ds11.transaction
-where created_at between '2018-10-01 00:00:00' and '2018-12-31 23:59:59'
+where created_at between '2018-10-01 00:00:00' and '2018-12-31 23:59:59' and store_id = '2'
 group by product_id
 having sum(quantity) > avg(quantity) 
 order by total_transactions desc
 limit 5;
 
-select s.type 
-from datasource_sql_ds11.store as s
-inner join (
-	select quantity, sum(quantity) as total_transactions 
-	from datasource_sql_ds11.transaction as t
-		inner join (
-			select city
-			from datasource_sql_ds11.customer 
-			where city = 'Jakarta'
-			) as c
-		on t.customer_id = c.id
-	group by quantity
-	limit 1)
+select 
+case
+	when city = 'Jakarta' then 'Jakarta'
+	else 'Luar Jakarta'
+end as region, 
+case
+	when gender = 'Male' then 'Male'
+	else 'Female'
+end as gender,
+t.type,
+sum(t.quantity) as total_transaction
+from datasource_sql_ds11.customer  
+join 
+	(
+	select s.type, customer_id, sum(quantity) as quantity
+	from datasource_sql_ds11.transaction  
+	join 
+		(
+		select id, type
+		from datasource_sql_ds11.store  
+		) as s
+	on store_id = s.id
+	group by s.type, customer_id
+	) as t
+on id = t.customer_id
+group by t.type, gender, region
+order by sum(t.quantity) desc
+limit 4;
